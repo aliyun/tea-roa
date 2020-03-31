d64d444599ae4f7c14e7288c49ff2296b11a0e7e
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-03-31 17:43:00 +0800

    generate php code

---
+++ php/src/Roa/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+namespace AlibabaCloud\Tea\Roa\Roa;
+
+use AlibabaCloud\Tea\Model;
+
+class Config extends Model{
+    public $accessKeyId;
+    public $accessKeySecret;
+    public $type;
+    public $securityToken;
+    public $protocol;
+    public $readTimeout;
+    public $connectTimeout;
+    public $httpProxy;
+    public $httpsProxy;
+    public $endpoint;
+    public $noProxy;
+    public $maxIdleConns;
+}
