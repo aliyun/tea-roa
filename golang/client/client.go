683112621145a6624ae3daf826b7849052d82a0f
wb-wzc505509 <wb-wzc505509@alibaba-inc.com>
2020-07-24 17:08:42 +0800

    support dorequest with action

--- golang/client/client.go
+++ golang/client/client.go
@@ -15,23 +15,40 @@ import (
  * Model for initing client
  */
 type Config struct {
-	AccessKeyId     *string               `json:"accessKeyId,omitempty" xml:"accessKeyId,omitempty"`
-	AccessKeySecret *string               `json:"accessKeySecret,omitempty" xml:"accessKeySecret,omitempty"`
-	SecurityToken   *string               `json:"securityToken,omitempty" xml:"securityToken,omitempty"`
-	Protocol        *string               `json:"protocol,omitempty" xml:"protocol,omitempty"`
-	RegionId        *string               `json:"regionId,omitempty" xml:"regionId,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
-	ReadTimeout     *int                  `json:"readTimeout,omitempty" xml:"readTimeout,omitempty"`
-	ConnectTimeout  *int                  `json:"connectTimeout,omitempty" xml:"connectTimeout,omitempty"`
-	HttpProxy       *string               `json:"httpProxy,omitempty" xml:"httpProxy,omitempty"`
-	HttpsProxy      *string               `json:"httpsProxy,omitempty" xml:"httpsProxy,omitempty"`
-	Credential      credential.Credential `json:"credential,omitempty" xml:"credential,omitempty"`
-	Endpoint        *string               `json:"endpoint,omitempty" xml:"endpoint,omitempty"`
-	NoProxy         *string               `json:"noProxy,omitempty" xml:"noProxy,omitempty"`
-	UserAgent       *string               `json:"userAgent,omitempty" xml:"userAgent,omitempty"`
-	MaxIdleConns    *int                  `json:"maxIdleConns,omitempty" xml:"maxIdleConns,omitempty"`
-	Network         *string               `json:"network,omitempty" xml:"network,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
-	Suffix          *string               `json:"suffix,omitempty" xml:"suffix,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
+	// accesskey id
+	AccessKeyId *string `json:"accessKeyId,omitempty" xml:"accessKeyId,omitempty"`
+	// accesskey secret
+	AccessKeySecret *string `json:"accessKeySecret,omitempty" xml:"accessKeySecret,omitempty"`
+	// security token
+	SecurityToken *string `json:"securityToken,omitempty" xml:"securityToken,omitempty"`
+	// http protocol
+	Protocol *string `json:"protocol,omitempty" xml:"protocol,omitempty"`
+	// region id
+	RegionId *string `json:"regionId,omitempty" xml:"regionId,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
+	// read timeout
+	ReadTimeout *int `json:"readTimeout,omitempty" xml:"readTimeout,omitempty"`
+	// connect timeout
+	ConnectTimeout *int `json:"connectTimeout,omitempty" xml:"connectTimeout,omitempty"`
+	// http proxy
+	HttpProxy *string `json:"httpProxy,omitempty" xml:"httpProxy,omitempty"`
+	// https proxy
+	HttpsProxy *string `json:"httpsProxy,omitempty" xml:"httpsProxy,omitempty"`
+	// credential
+	Credential credential.Credential `json:"credential,omitempty" xml:"credential,omitempty"`
+	// endpoint
+	Endpoint *string `json:"endpoint,omitempty" xml:"endpoint,omitempty"`
+	// proxy white list
+	NoProxy *string `json:"noProxy,omitempty" xml:"noProxy,omitempty"`
+	// user agent
+	UserAgent *string `json:"userAgent,omitempty" xml:"userAgent,omitempty"`
+	// max idle conns
+	MaxIdleConns *int `json:"maxIdleConns,omitempty" xml:"maxIdleConns,omitempty"`
+	// network for endpoint
+	Network *string `json:"network,omitempty" xml:"network,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
+	// suffix for endpoint
+	Suffix *string `json:"suffix,omitempty" xml:"suffix,omitempty" pattern:"^[a-zA-Z0-9_-]+$"`
 	// Deprecated
+	// credential type
 	Type *string `json:"type,omitempty" xml:"type,omitempty"`
 }
 
@@ -268,6 +285,146 @@ func (client *Client) DoRequest(version *string, protocol *string, method *strin
 				"x-acs-signature-method":  tea.String("HMAC-SHA1"),
 				"x-acs-signature-version": tea.String("1.0"),
 				"x-acs-version":           version,
+				"user-agent":              util.GetUserAgent(client.UserAgent),
+				// x-sdk-client': helper.DEFAULT_CLIENT
+			}, headers)
+			if !tea.BoolValue(util.IsUnset(body)) {
+				request_.Body = tea.ToReader(util.ToJSONString(body))
+				request_.Headers["content-type"] = tea.String("application/json; charset=UTF-8;")
+			}
+
+			if !tea.BoolValue(util.IsUnset(query)) {
+				request_.Query = query
+			}
+
+			if !tea.BoolValue(util.EqualString(authType, tea.String("Anonymous"))) {
+				accessKeyId, _err := client.Credential.GetAccessKeyId()
+				if _err != nil {
+					return _result, _err
+				}
+
+				accessKeySecret, _err := client.Credential.GetAccessKeySecret()
+				if _err != nil {
+					return _result, _err
+				}
+
+				securityToken, _err := client.Credential.GetSecurityToken()
+				if _err != nil {
+					return _result, _err
+				}
+
+				if !tea.BoolValue(util.Empty(securityToken)) {
+					request_.Headers["x-acs-accesskey-id"] = accessKeyId
+					request_.Headers["x-acs-security-token"] = securityToken
+				}
+
+				stringToSign := roautil.GetStringToSign(request_)
+				request_.Headers["authorization"] = tea.String("acs " + tea.StringValue(accessKeyId) + ":" + tea.StringValue(roautil.GetSignature(stringToSign, accessKeySecret)))
+			}
+
+			response_, _err := tea.DoRequest(request_, _runtime)
+			if _err != nil {
+				return _result, _err
+			}
+			if tea.BoolValue(util.EqualNumber(response_.StatusCode, tea.Int(204))) {
+				_result = make(map[string]interface{})
+				_err = tea.Convert(map[string]map[string]*string{
+					"headers": response_.Headers,
+				}, &_result)
+				return _result, _err
+			}
+
+			result, _err := util.ReadAsJSON(response_.Body)
+			if _err != nil {
+				return _result, _err
+			}
+
+			if tea.BoolValue(util.Is4xx(response_.StatusCode)) || tea.BoolValue(util.Is5xx(response_.StatusCode)) {
+				err := util.AssertAsMap(result)
+				_err = tea.NewSDKError(map[string]interface{}{
+					"code":    tea.ToString(DefaultAny(err["Code"], err["code"])) + "Error",
+					"message": "code: " + tea.ToString(tea.IntValue(response_.StatusCode)) + ", " + tea.ToString(DefaultAny(err["Message"], err["message"])) + " requestid: " + tea.ToString(DefaultAny(err["RequestId"], err["requestId"])),
+					"data":    err,
+				})
+				return _result, _err
+			}
+
+			_result = make(map[string]interface{})
+			_err = tea.Convert(map[string]interface{}{
+				"headers": response_.Headers,
+				"body":    result,
+			}, &_result)
+			return _result, _err
+		}()
+		if !tea.BoolValue(tea.Retryable(_err)) {
+			break
+		}
+	}
+
+	return _resp, _err
+}
+
+/**
+ * Encapsulate the request and invoke the network
+ * @param action api name
+ * @param version product version
+ * @param protocol http or https
+ * @param method e.g. GET
+ * @param authType when authType is Anonymous, the signature will not be calculate
+ * @param pathname pathname of every api
+ * @param query which contains request params
+ * @param headers request headers
+ * @param body content of request
+ * @param runtime which controls some details of call api, such as retry times
+ * @return the response
+ */
+func (client *Client) DoRequestWithAction(action *string, version *string, protocol *string, method *string, authType *string, pathname *string, query map[string]*string, headers map[string]*string, body interface{}, runtime *util.RuntimeOptions) (_result map[string]interface{}, _err error) {
+	_err = tea.Validate(runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_runtime := map[string]interface{}{
+		"timeouted":      "retry",
+		"readTimeout":    tea.IntValue(util.DefaultNumber(runtime.ReadTimeout, client.ReadTimeout)),
+		"connectTimeout": tea.IntValue(util.DefaultNumber(runtime.ConnectTimeout, client.ConnectTimeout)),
+		"httpProxy":      tea.StringValue(util.DefaultString(runtime.HttpProxy, client.HttpProxy)),
+		"httpsProxy":     tea.StringValue(util.DefaultString(runtime.HttpsProxy, client.HttpsProxy)),
+		"noProxy":        tea.StringValue(util.DefaultString(runtime.NoProxy, client.NoProxy)),
+		"maxIdleConns":   tea.IntValue(util.DefaultNumber(runtime.MaxIdleConns, client.MaxIdleConns)),
+		"retry": map[string]interface{}{
+			"retryable":   tea.BoolValue(runtime.Autoretry),
+			"maxAttempts": tea.IntValue(util.DefaultNumber(runtime.MaxAttempts, tea.Int(3))),
+		},
+		"backoff": map[string]interface{}{
+			"policy": tea.StringValue(util.DefaultString(runtime.BackoffPolicy, tea.String("no"))),
+			"period": tea.IntValue(util.DefaultNumber(runtime.BackoffPeriod, tea.Int(1))),
+		},
+		"ignoreSSL": tea.BoolValue(runtime.IgnoreSSL),
+	}
+
+	_resp := make(map[string]interface{})
+	for _retryTimes := 0; tea.BoolValue(tea.AllowRetry(_runtime["retry"], tea.Int(_retryTimes))); _retryTimes++ {
+		if _retryTimes > 0 {
+			_backoffTime := tea.GetBackoffTime(_runtime["backoff"], tea.Int(_retryTimes))
+			if tea.IntValue(_backoffTime) > 0 {
+				tea.Sleep(_backoffTime)
+			}
+		}
+
+		_resp, _err = func() (map[string]interface{}, error) {
+			request_ := tea.NewRequest()
+			request_.Protocol = util.DefaultString(client.Protocol, protocol)
+			request_.Method = method
+			request_.Pathname = pathname
+			request_.Headers = tea.Merge(map[string]*string{
+				"date":                    util.GetDateUTCString(),
+				"host":                    client.EndpointHost,
+				"accept":                  tea.String("application/json"),
+				"x-acs-signature-nonce":   util.GetNonce(),
+				"x-acs-signature-method":  tea.String("HMAC-SHA1"),
+				"x-acs-signature-version": tea.String("1.0"),
+				"x-acs-version":           version,
+				"x-acs-action":            action,
 				"user-agent":              util.GetUserAgent(client.UserAgent),
 				// x-sdk-client': helper.DEFAULT_CLIENT
 			}, headers)
