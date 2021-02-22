5861120941d2f3069c00ee914a41c7b1d219aead
yndu13 <yndu13@gmail.com>
2021-02-22 22:02:22 +0800

    update TeaUnretryableException

--- java/src/main/java/com/aliyun/tearoa/models/Config.java
+++ java/src/main/java/com/aliyun/tearoa/models/Config.java
@@ -3,8 +3,6 @@ package com.aliyun.tearoa.models;
 
 import com.aliyun.tea.*;
 
-import java.lang.reflect.InvocationTargetException;
-
 /**
  * Model for initing client
  */
@@ -81,13 +79,162 @@ public class Config extends TeaModel {
     @Deprecated
     public String type;
 
-    public static Config build(java.util.Map<String, ?> map) {
+    public static Config build(java.util.Map<String, ?> map) throws Exception {
         Config self = new Config();
-        try {
-            return TeaModel.build(map, self);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return TeaModel.build(map, self);
+    }
+
+    public Config setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+        return this;
+    }
+
+    public String getAccessKeyId() {
+        return this.accessKeyId;
+    }
+
+    public Config setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+        return this;
+    }
+
+    public String getAccessKeySecret() {
+        return this.accessKeySecret;
+    }
+
+    public Config setSecurityToken(String securityToken) {
+        this.securityToken = securityToken;
+        return this;
+    }
+
+    public String getSecurityToken() {
+        return this.securityToken;
+    }
+
+    public Config setProtocol(String protocol) {
+        this.protocol = protocol;
+        return this;
+    }
+
+    public String getProtocol() {
+        return this.protocol;
+    }
+
+    public Config setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public Config setReadTimeout(Integer readTimeout) {
+        this.readTimeout = readTimeout;
+        return this;
+    }
+
+    public Integer getReadTimeout() {
+        return this.readTimeout;
+    }
+
+    public Config setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+        return this;
+    }
+
+    public Integer getConnectTimeout() {
+        return this.connectTimeout;
+    }
+
+    public Config setHttpProxy(String httpProxy) {
+        this.httpProxy = httpProxy;
+        return this;
+    }
+
+    public String getHttpProxy() {
+        return this.httpProxy;
+    }
+
+    public Config setHttpsProxy(String httpsProxy) {
+        this.httpsProxy = httpsProxy;
+        return this;
+    }
+
+    public String getHttpsProxy() {
+        return this.httpsProxy;
+    }
+
+    public Config setCredential(com.aliyun.credentials.Client credential) {
+        this.credential = credential;
+        return this;
+    }
+
+    public com.aliyun.credentials.Client getCredential() {
+        return this.credential;
+    }
+
+    public Config setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+        return this;
+    }
+
+    public String getEndpoint() {
+        return this.endpoint;
+    }
+
+    public Config setNoProxy(String noProxy) {
+        this.noProxy = noProxy;
+        return this;
+    }
+
+    public String getNoProxy() {
+        return this.noProxy;
+    }
+
+    public Config setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+        return this;
+    }
+
+    public String getUserAgent() {
+        return this.userAgent;
+    }
+
+    public Config setMaxIdleConns(Integer maxIdleConns) {
+        this.maxIdleConns = maxIdleConns;
+        return this;
+    }
+
+    public Integer getMaxIdleConns() {
+        return this.maxIdleConns;
+    }
+
+    public Config setNetwork(String network) {
+        this.network = network;
+        return this;
+    }
+
+    public String getNetwork() {
+        return this.network;
+    }
+
+    public Config setSuffix(String suffix) {
+        this.suffix = suffix;
+        return this;
+    }
+
+    public String getSuffix() {
+        return this.suffix;
+    }
+
+    public Config setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getType() {
+        return this.type;
     }
 
 }
